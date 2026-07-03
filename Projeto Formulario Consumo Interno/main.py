8c9412e4ec301dab040b7fec61646a18606f13e8
RodrigoPeres49 <130695825+RodrigoPeres49@users.noreply.github.com>
2026-07-03 13:29:37 -0300

    Delete Projeto Formulario Consumo Interno directory

--- Projeto Formulario Consumo Interno/main.py
+++
@@ -1,9 +0,0 @@
-import webview
-import os
-from api import *
-
-if __name__ == '__main__':
-    html = os.path.abspath("templates/index.html")
-    api = Api()
-    webview.create_window("Consumo Interno", f"file://{html}", js_api=api, maximized=True)
-    webview.start()
